4c07f5a6156d015f11425a6c215f726e7a7c3a42
juniorricardo <neo.junior.16@gmail.com>
2020-06-19 23:43:08 -0300

    First request -> Posts

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,23 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(i => new Post
+            {
+                PostId = i,
+                UserId = i * 2,
+                Description = $"Description {i}",
+                Date = DateTime.Now,
+                Image = $"https://migaleria.com/selectImage/id/{i}"
+            });
+            return posts;
+        }
+    }
+}
